8055e607f7858fbb0deeb5efe8002645ec969e9a
小白 <xiaobai@xiaobaideMBP.lan>
2015-10-21 14:10:10 +0800

    first commit

---
+++ xinlang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xinlang
+//
+//  Created by 小白 on 15/10/21.
+//  Copyright (c) 2015年 小白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xinlang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinlang
+//
+//  Created by 小白 on 15/10/21.
+//  Copyright (c) 2015年 小白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
